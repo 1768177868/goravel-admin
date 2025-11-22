aeb8d6550f2e24742cb5db030db13813d98ade5c
wangxuancheng <wangxuancheng123@gmail.com>
2025-11-22 20:12:23 +0800

    填充

--- database/kernel.go
+++ database/kernel.go
@@ -37,5 +37,6 @@ func (kernel Kernel) Migrations() []schema.Migration {
 func (kernel Kernel) Seeders() []seeder.Seeder {
 	return []seeder.Seeder{
 		&seeders.DatabaseSeeder{},
+		&seeders.AdminSeeder{},
 	}
 }
