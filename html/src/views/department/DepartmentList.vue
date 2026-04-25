6c4b10eee901a0f36620690ea92235cb3d9d7d39
wangxuancheng <wangxuancheng123@gmail.com>
2026-04-25 17:49:47 +0800

    优化

--- html/src/views/department/DepartmentList.vue
+++ html/src/views/department/DepartmentList.vue
@@ -44,6 +44,7 @@
       <TableToolbar
         :on-refresh="handleRefresh"
         fullscreen-target=".list-page"
+        :show-column-setting-btn="false"
         :visible-columns="visibleColumns"
         :all-columns="allTableColumns"
         :default-visible-columns="defaultVisibleColumns"
