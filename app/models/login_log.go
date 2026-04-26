e965a91cb817d9af3820ab76258c459b7387f2e7
wangxuancheng <wangxuancheng123@gmail.com>
2026-04-26 11:07:41 +0800

    登录日志状态

--- app/models/login_log.go
+++ app/models/login_log.go
@@ -12,7 +12,7 @@ type LoginLog struct {
 	IP        string `gorm:"size:50;comment:IP地址"`
 	UserAgent string `gorm:"size:500;comment:用户代理"`
 	Location  string `gorm:"size:100;comment:登录地点"`
-	Status    uint8  `gorm:"default:1;comment:状态 1:成功 0:失败"`
+	Status    uint8  `gorm:"comment:状态 1:成功 0:失败"`
 	Message   string `gorm:"size:255;comment:登录信息"`
 	Request   string `gorm:"type:text;comment:请求数据"`
 }
